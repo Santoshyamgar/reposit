80ac3002134625fb6e55051b58054d60bd89ef4f
santoshyamgar <yamgarsantosh45@gmail.com>
2022-01-25 17:20:36 +0530

    first commit

---
+++ constructor.py
@@ -0,0 +1,16 @@
+class test:
+    def __init__(self):
+        print("no arg constructer")
+
+    def __int__(self,a):
+        print("one arg constructre")
+
+    def __init__(self,b,c):
+        print("tow arg constructer")
+
+
+#t1 = test()
+t1=test(10,20)
+
+
+
